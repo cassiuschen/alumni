39552e9f00f428c17c7a81dcbab638156f5793fe
cassiuschen <chzsh1995@gmail.com>
2014-12-25 13:15:35 +0800

    add regions

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,18 +9,27 @@ class User
   field :email,              type: String, default: ""
   field :encrypted_password, type: String, default: ""
 
+  field :mobile,             type: String
+
   # Main Data
-  # BDFZerID格式： "20140010001", 毕业年份 2014， 所在地 北京（001）， 序号 001
-  field :BdfzerId,             type: String
+  # BDFZerID格式： "201400100001", 毕业年份 2014， 所在地 北京（001）， 序号 001
+  field :BdfzerId,             type: String, default: -> {
+    guaduate_at + region.to_s.rjust(3, '0') + _id.to_s.rjust(5, '0')
+  }
   validates :BdfzerId,         presence: true, uniqueness: true
 
-  field :region,               type: Integer, default: 084
+  field :region,               type: Integer, default: 84
 
   field :sexual,               type: String, default: 'Unknown'
 
-  field :guaduate_at,          type: String
+  field :guaduate_at,          type: String, default: -> { Time.now.year.to_s }
 
+  field :department,           type: Integer, default: 0
+  field :department_member,    type: Boolean, default: false
 
+  field :name,                 type: String
+
+  field :power,                type: Integer, default: 0
 
   ## Recoverable
   field :reset_password_token,   type: String
@@ -47,5 +56,45 @@ class User
   # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
   # field :locked_at,       type: Time
 
+  REGIONS = {
+    84  => 'Unknown',
+    1   => '北京总部',
+    10  => '江浙沪地区分部',
+    20  => '东北地区分部',   #包括辽宁、吉林、黑龙江
+    30  => '华东南地区分部', #包括山东、安徽、福建、广东、广西、海南
+    40  => '华北地区分部',   #包括天津、河北、山西、内蒙古
+    50  => '西部地区分部',   #包括宁夏、新疆、青海、陕西、甘肃、四川、云南、贵州、西藏、重庆
+    60  => '华中地区分部',   #包括湖北、湖南、河南、江西
+    70  => '台港澳地区分部',
+    100 => '北美地区分部',
+    200 => '欧洲分部',
+    300 => '南半球分部'
+  }
+ 
+  def department_member?
+    !!(department_member)
+  end
+
+  def zone
+    User::REGIONS[region]
+  end
+
+  def role
+    case power
+    when 0 then 'alumni'
+    when 1 then 'secretary'
+    when 2 then 'manager'
+    when 3 then 'admin'
+    end
+  end
+
+  def self.create_from_angular_params(params)
+    @user = User.create {
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password],
+      mobile: params[:mobile],
+      guaduate_at: params[:guaduate_at],
+    }
 
 end
