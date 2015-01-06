bf2aa4978d61715b4fff4933b1dea76eab5f8dfc
cassiuschen <chzsh1995@gmail.com>
2015-01-07 03:46:00 +0800

    init an api for check data

--- app/controllers/api/v1/register_controller.rb
+++ app/controllers/api/v1/register_controller.rb
@@ -19,4 +19,20 @@ class Api::V1::RegisterController < Api::V1::BaseController
       }
     end
   end
+
+  def get_list
+    @list = []
+    User.all.each do |u|
+      @list << {
+        name: u.name,
+        graduateAt: u.graduate_at,
+        zone: u.zone,
+        unit: u.unit
+      }
+    end
+    render json: {
+      size: User.all.size,
+      data: @list
+    }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post 'user' => 'register#create'
+      get 'list' => 'register#get_list'
     end
   end
 
